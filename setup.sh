6213c0b53ed0cfd39bd0c4eaaec5b39b0bbc1397
Olivier Steck <osteck@gmail.com>
2019-02-22 17:42:18 -0500

    First Commit

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+PROJECT=persistency
+
+rm -rf /Users/oliviersteck/Documents/sources/python/hopla_root/${PROJECT}/build
+
+echo "vanilla env"
+echo
+python_bin=/usr/local/anaconda3/envs/vanilla/bin/python
+pip_bin=/usr/local/anaconda3/envs/vanilla/bin/pip
+${pip_bin} uninstall ${PROJECT} -y
+${pip_bin} install .
+${python_bin} setup.py config --config-dir /Users/oliviersteck/Documents/sources/python/hopla_root/${PROJECT}/resources/config
+${python_bin}  setup.py install --force; python setup.py test
+echo "---------------------------------------------------------------------------------------------------------------------------------------"
+
+echo
+
+echo "hopla env"
+echo
+python_bin=/usr/local/anaconda3/envs/hopla/bin/python
+pip_bin=/usr/local/anaconda3/envs/hopla/bin/pip
+${pip_bin} uninstall ${PROJECT} -y
+${pip_bin} install .
+${python_bin}  setup.py config --config-dir /Users/oliviersteck/Documents/sources/python/hopla_root/${PROJECT}/resources/config
+${python_bin}  setup.py install --force; python setup.py test
+echo "---------------------------------------------------------------------------------------------------------------------------------------"
---
+++ tests/persistency/run.py
@@ -0,0 +1,22 @@
+from hopla.base.graphs.nodes.node import Node
+from hopla.persistency.dal.neo4j import CypherOperations
+from hopla.persistency.generators import NodeCommands
+
+n1 = Node({"test": {"value": "ABC"}})
+CypherOperations.create_node(n1)
+nsg1 = NodeCommands(n1)
+
+n2 = Node({"test": {"value": "BCD"}})
+CypherOperations.create_node(n2)
+nsg2 = NodeCommands(n2)
+
+r1 = (n1 < n2)(rel_type="buddies_ole_boys",
+               data={"how": {"we": "test"}, "the depth": "of", "our souls": {"is": "a mystery"}})
+# matches = "{m1}, {m2}".format(
+#     m1=nsg1.self_generate_match(excluded=["__create_date", "__update_date"], variable="n1"),
+#     m2=nsg2.self_generate_match(excluded=["__create_date", "__update_date"], variable="n1"))
+# print(RelationshipCommands(r1).self_generate_create(variable="r1"))
+
+# result = CypherOperations.create_nodes_relationship(n1, n2, rel_type="BUDDINSKY", variable='r')
+result = CypherOperations.delete_all() #create_relationship(r1, variable='r')
+result
